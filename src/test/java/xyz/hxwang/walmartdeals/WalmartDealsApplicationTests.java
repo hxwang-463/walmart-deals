17d41aab174ca144a443241c32a45e057798b66f
Huixuan Wang <whx987172@gmail.com>
2024-02-05 17:23:39 -0500

    finish basic functions

--- src/test/java/xyz/hxwang/walmartdeals/WalmartDealsApplicationTests.java
+++ src/test/java/xyz/hxwang/walmartdeals/WalmartDealsApplicationTests.java
@@ -3,11 +3,11 @@ package xyz.hxwang.walmartdeals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WalmartDealsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class WalmartDealsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
